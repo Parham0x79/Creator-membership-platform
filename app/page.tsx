cc0c46ba41cee808f207c5bb0147fb931d23e9ce
Parham <parhamfatemi2000@gmail.com>
2026-01-30 22:25:12 +0330

    latest update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <main>
-        <h1>Welcome to the Home Page</h1>
+      <main className="p-5 border m-2">
+        <h1 className="text-2xl text-blue-300 italic">
+          Welcome to the Home Page
+        </h1>
       </main>
     </div>
   );
